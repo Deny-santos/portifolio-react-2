f4ed6a8f03c814591b8ed48b26c39cc8015b97a9
denisson <denisson.graca3@gmail.com>
2022-12-20 20:22:04 -0300

    header ok

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,24 +2,26 @@ import React from 'react'
 import "./Header.css"
 import CTA from './CTA'
 import HeaderSocial from './HeaderSocial'
-import { perfilJPG } from "../../assets"
+import { perfilPNG } from "../../assets"
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello I`m</h5>
+        <h5>Olá, Sou O </h5>
         <h1>Deny Santos</h1>
-        <h5 className="text-light">Web Developer</h5>
+        <h5 className="text-light">Desenvolvedor Web Frontend</h5>
         <CTA/>
         <HeaderSocial/>
 
-        
         <div className="me">
-          <img src={perfilJPG} alt="me" />
+          <img src={perfilPNG} alt="me" />
         </div>
 
-        <a className='scroll_down' href="#contact" >Scroll Down</a>
+        <div className='scroll_wrapp'>
+          <div></div>
+          <a className='scroll_down' href="#contact" >Ir Para Baixo</a>
+        </div>
       </div>
     </header>
   )
